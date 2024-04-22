8bd5f013ec55d9ce52b61c0b4c6dedb30bf53976
sbahaddi <s-bahaddi@autodistribution.com>
2024-04-22 17:09:09 +0100

    add lodash-es

--- src/app/components/demo.css.ts
+++ src/app/components/demo.css.ts
@@ -1,5 +1,8 @@
 import { style } from "@vanilla-extract/css";
+import { cloneDeep } from "lodash-es";
+
+const test = cloneDeep;
 
 export const demo = style({
-    color: 'red'
-})
+  color: "red",
+});
